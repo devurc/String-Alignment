99b86414cba2ffbadbdd0b87746796499f9cc65f
Eric <Eric@Sarahs-MacBook-Air.local>
2017-05-01 17:44:04 +0200

    Refactored score to Char -> Char -> Int and added comments

--- StringAlignment.hs
+++ StringAlignment.hs
@@ -5,21 +5,24 @@ scoreMatch = 0
 scoreMismatch = (-1)
 scoreSpace = (-1)
 
-score :: (Char,Char) -> Int
-score (x, y) 
+type AlignmentType = (String,String)
+
+score :: Char -> Char -> Int
+score x y
   | x == '-' || y == '-' = scoreSpace
   | x == y               = scoreMatch
   | x /= y               = scoreMismatch
 
 
+-- 2.a) Calculates optimal score for two input strings
 similarityScore :: String -> String -> Int
 similarityScore [] [] = 0
-similarityScore [] (y:ys) = similarityScore [] ys + score('-',y)
-similarityScore (x:xs) [] = similarityScore xs [] + score(x,'-')
+similarityScore [] (y:ys) = similarityScore [] ys + score '-' y
+similarityScore (x:xs) [] = similarityScore xs [] + score x '-'
 similarityScore (x:xs) (y:ys) =
-  let first = similarityScore xs ys + score(x,y)
-      second = similarityScore xs (y:ys) + score(x,'-')
-      third = similarityScore (x:xs) ys + score('-',y)
+  let first = similarityScore xs ys + score x y
+      second = similarityScore xs (y:ys) + score x '-'
+      third = similarityScore (x:xs) ys + score '-' y
   in max first $ max second third
   
 
@@ -28,15 +31,17 @@ similarityScore (x:xs) (y:ys) =
 attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])] 
 attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
 
-
+-- 2.c) Determines highest value present in list, and filters list
+-- based on that high value
 maximaBy :: Ord b => (a -> b) -> [a] -> [a] 
 maximaBy valueFcn xs =
   -- [ x | x<-xs, valueFcn x == highestVal ]
   filter (\x -> valueFcn x == highestVal) xs
   where highestVal = head . last . group . sort . map valueFcn $ xs
 
-
-
+-- 2.d) Prints all optimal alignments between string1 and string2
+optAlignments :: String -> String -> [AlignmentType]
+optAlignments string1 string2 = []
 
 
 

a7bf995fa2548c3732caf30df2614f3d78bb98db
Cleo Schurink <Cleo Schurink>
2017-05-10 10:23:16 +0200

    Opt table implemented

--- a2.hs
+++ a2.hs
@@ -1,31 +1,121 @@
-scoreMatch = 1
+import
+  Data.List
+
+scoreMatch = 0
 scoreMismatch = (-1)
-scoreSpace = (-2)
+scoreSpace = (-1)
 
-similarityScore :: String -> String -> Int
+type AlignmentType = (String,String)
 
-similarityScore string1 string2 = score (length string1) (length string2)
-    where
-        --extracting the similarity score from the table using indexing
-        score i j = scoreTable!!i!!j
+score :: Char -> Char -> Int
+score x y
+  | x == '-' || y == '-' = scoreSpace
+  | x == y               = scoreMatch
+  | x /= y               = scoreMismatch
 
-        --this line creates the score table and simultaneously populates it with the scores
-        scoreTable = [[scoreEntry i j | j <- [0..]] | i <- [0..]]
+optSimilarityScore :: String -> String -> Int
+optSimilarityScore string1 string2 = optLen (length string1) (length string2)
+  where
+    --string1 = reverse string1
+    --string2 = reverse string2
+    optLen i j = optTable!!i!!j
+    optTable = [[optEntry i j | j<-[0..]] | i<-[0..]]
+
+    optEntry :: Int -> Int -> Int
+    optEntry 0 0 = 0
+    optEntry i 0 = scoreSpace * i
+    optEntry 0 j = scoreSpace * j
+    optEntry i j = maximum [score x y + optLen (i-1) (j-1), --adding to the score thats top left of current cell
+                            score '-' y + optLen i (j-1), --adding to the score above current cell
+                            score x '-' + optLen (i-1) j] --adding to the score to the left of current cell
+
+      where x = string1!!(i-1)
+            y = string2!!(j-1)
+
+duplicate :: String -> Int -> String
+duplicate string n = concat $ replicate n string
+
+--newOptAlignments :: String -> String -> [AlignmentType]
+--newOptAlignments string1 string2 = algLen (length string1) (length string2)
+  --where
+    --string1 = reverse string1
+    --string2 = reverse string2
+
+    --algLen i j = algTable!!i!!j
+    --algTable = [[algEntry i j | j<-[0..]] | i<-[0..]]
+
+    --algEntry :: Int -> Int -> (Int, [AlignmentType])
+    --algEntry 0 0 = (0, [([],[])])
+    --algEntry i 0 = (optLen i 0, ([take i string1], [duplicate '-' i]))
+    --algEntry 0 j = (optLen 0 j, ([duplicate '-' i], [take i string1]))
+    --algEntry i j = (optLen i j, concatMap snd z)
+        --where
+            --
 
 
-        --All intersections in the table with an empty list has a score of 0
-        scoreEntry _ 0 = 0
-        scoreEntry 0 _ = 0
 
-        --Else if a list is not empty, then apply the scoring criteria to the two sublists/list
-        --Also accumulate score from top left diagonal
+-- 2.a) Calculates optimal score for two input strings
+similarityScore :: (String, String) -> Int
+similarityScore ([], []) = 0
+similarityScore ([], (y:ys)) = similarityScore ([], ys) + score '-' y
+similarityScore ((x:xs), []) = similarityScore (xs, []) + score x '-'
+similarityScore ((x:xs), (y:ys)) =
+  let first = similarityScore(xs, ys) + score x y
+      second = similarityScore(xs, (y:ys)) + score x '-'
+      third = similarityScore((x:xs), ys) + score '-' y
+  in max first $ max second third
 
-        scoreEntry i j
-            | h1 == h2 = scoreMatch + score(i-1, j-1)
-            | h1 == '_' || h2 == '_' = scoreSpace + score(i-1, j-1)
-            | h1 /= h2 = scoreMismatch + score(i-1, j-1)
 
-        --h1 is the head of string1 where
-            where
-                h1 = string1!!(i-1)
-                h2 = string2!!(j-1)
+-- 2.b) This function takes two arguments, and respectively attaches them to the front
+-- of a pair of two lists, for every pair in the list of pairs
+attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
+attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
+
+-- 2.c) Determines highest value present in list, and filters list
+-- based on that high value
+maximaBy :: Ord b => (a -> b) -> [a] -> [a]
+maximaBy valueFcn xs =
+  -- [ x | x<-xs, valueFcn x == highestVal ]
+  filter (\x -> valueFcn x == highestVal) xs
+  where highestVal = head . last . group . sort . map valueFcn $ xs
+
+-- 2.d) Returns a list of all optimal alignments between string1 and string 2
+optAlignments :: String -> String -> [AlignmentType]
+optAlignments string1 string2 =
+  maximaBy calcScore $ generate string1 string2
+
+-- Helper function that generates all possible alignments of
+-- a pair of two strings
+generate :: String -> String -> [AlignmentType]
+generate [] []     = [([], [])]
+generate (x:xs) [] = attachHeads x '-' $ generate xs []
+generate [] (y:ys) = attachHeads '-' y $ generate [] ys
+generate (x:xs) (y:ys) =
+  attachHeads x y (generate xs ys) ++
+  attachHeads x '-' (generate xs (y:ys)) ++
+  attachHeads '-' y (generate (x:xs) ys)
+
+
+-- Helper function that calculates similarity score for a given
+-- pair of strings
+calcScore :: (String, String) -> Int
+calcScore ([], []) = 0
+calcScore ((x:xs), (y:ys))
+  | x == '-' || y == '-' = scoreSpace + calcScore(xs, ys)
+  | x == y               = scoreMatch + calcScore(xs, ys)
+  | x /= y               = scoreMismatch + calcScore(xs, ys)
+
+
+-- 2.e) Function that outputs best possible arrangements in
+-- readable fashion
+outputOptAlignments :: String -> String -> IO()
+outputOptAlignments string1 string2 =
+  putStrLn ("There are " ++ show (numResults)
+  ++ " optimal alignments: \n\n" ++ concat formatResult
+  ++ "There were " ++ show (numResults) ++ " optimal alignments!")
+  where result = optAlignments string1 string2
+        numResults = length result
+        firsts = map (intersperse ' ') $ map fst result
+        seconds = map (intersperse ' ') $ map snd result
+        newResult = zip firsts seconds
+        formatResult = [ x ++ "\n" ++ y ++ "\n\n" | (x,y)<-newResult ]
